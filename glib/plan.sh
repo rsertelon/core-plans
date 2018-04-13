eb5fe8cde8698c3e2b0b4cb33f500ff00b97be11
Romain Sertelon <romain@sertelon.fr>
2018-04-13 21:44:56 +0200

    wip
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- glib/plan.sh
+++ glib/plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=core
 pkg_name=glib
-pkg_version="2.50.3"
+pkg_version="2.56.1"
 pkg_description="$(cat << EOF
   GLib is a general-purpose utility library, which provides many useful data
   types, macros, type conversions, string utilities, file utilities, a
@@ -12,7 +12,7 @@ pkg_source="https://download.gnome.org/sources/${pkg_name}/${pkg_version%.*}/${p
 pkg_license=('LGPL-2.0')
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_upstream_url="https://developer.gnome.org/glib/"
-pkg_shasum="82ee94bf4c01459b6b00cb9db0545c2237921e3060c0b74cff13fbc020cfd999"
+pkg_shasum="40ef3f44f2c651c7a31aedee44259809b6f03d3d20be44545cd7d177221c0b8d"
 pkg_deps=(
   core/coreutils
   core/elfutils
